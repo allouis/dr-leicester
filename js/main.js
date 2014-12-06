6900d44d34becdd7f2154d13f60d207bdd2ebe4a
benji6 <benhall22@hotmail.co.uk>
2014-12-06 23:47:15 +0000

    keyboard controls added

--- js/main.js
+++ js/main.js
@@ -1,50 +1,126 @@
 var Kinetic = require('kinetic');
+//var Events = require('minivents');
 
+//var pubsub = new Events();
+
+var windowWidth = window.innerWidth; //this is used to determine player velocity
+window.addEventListener('resize', resizeStage, false);
 var stage = new Kinetic.Stage({
 	container: 'container',
-	width: 512,
-	height: 512 
+	width: windowWidth,
+	height: window.innerHeight 
 });
 var layer = new Kinetic.Layer();
 
-var rect = new Kinetic.Rect({
-    name: 'rect',
+
+var resizeStage = function() {
+	windowWidth = window.innerWidth;
+	stage.setWidth(windowWidth);
+	stage.setHeight(window.innerHeight);
+};
+
+var background = new Kinetic.Rect({
+	x: 0,
+	y: 0,
+	width: stage.getWidth(),
+	height: stage.getHeight(),
+	fill: '#F0F',
+});
+layer.add(background);
+var comp = new Kinetic.Rect({
+    name: 'comp',
     x: 50,
     y: 50,
     width: 25,
     height: 25,
-    fill: 'black',
+    fill: 'blue',
     draggable: true
 });
-var circ = new Kinetic.Circle({
-    id: 'circ',
-    x: 100,
-    y: 100,
-    radius: 5,
-    fill: 'red'
+layer.add(comp);
+var player = new Kinetic.Rect({
+    name: 'player',
+    x: 50,
+    y: 50,
+    width: 25,
+    height: 25,
+    fill: 'green',
+    draggable: true
 });
+layer.add(player);
 
-layer.add(rect);
-layer.add(circ);
 stage.add(layer);
 
-var velMag = 256;
-var vel = velMag;
+var velMag = windowWidth / 8;
+var xVel;
+var yVel;
 var anim = new Kinetic.Animation(function(frame) {
+	var xDist;
+	var yDist;
 	var stageWidth = stage.getWidth();
-	var absPos = circ.getAbsolutePosition();
-	if (absPos.x >= stageWidth) {
-		vel = -velMag;
-	} else if (absPos.x <= 0) {
-		vel = velMag;
+	var absPos = player.getAbsolutePosition();
+	
+	if (xVel) {
+		xDist = xVel * (frame.timeDiff / 1000);
 	}
-	var xDist = vel * (frame.timeDiff / 1000);
-	var yDist = 0;
+	if (yVel) {
+		yDist = yVel * (frame.timeDiff / 1000);
+	}
 
-	circ.move({
+	player.move({
 		x: xDist,
 		y: yDist
 	});
 }, layer);
 
 anim.start();
+
+//controls
+(function () {
+	var keyCodes = [];
+
+	keyCodes[37] = keyCodes[65] = {
+		down: function() {
+			xVel = -velMag;
+		},
+		up: function() {
+			xVel = 0;
+		},
+	};
+	keyCodes[38] = keyCodes[87] = {
+		down: function() {
+			yVel = -velMag;
+		},
+		up: function() {
+			yVel = 0;
+		}
+	};
+	keyCodes[39] = keyCodes[68] = {
+		down: function() {
+			xVel = velMag;
+		},
+		up: function() {
+			xVel = 0;
+		}
+	}
+	keyCodes[40] = keyCodes[83] = {
+		down: function() {
+			yVel = velMag;
+		},
+		up: function() {
+			yVel = 0;
+		}
+	}
+
+	document.onkeydown = function(e) {
+		if (keyCodes[e.keyCode]) {
+			keyCodes[e.keyCode].down();
+		}
+	};
+	
+	document.onkeyup = function(e) {
+		if (keyCodes[e.keyCode]) {
+			keyCodes[e.keyCode].up();
+		}
+	}
+	
+}());
