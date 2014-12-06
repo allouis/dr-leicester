a6bb54b1382e4f192437879db7d1699ed2b79ffd
benji6 <benhall22@hotmail.co.uk>
2014-12-06 19:47:29 +0000

    initial view

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 var Kinetic = require('kinetic');
+
+var stage = new Kinetic.Stage({
+	container: 'container',
+	width: 512,
+	height: 512 
+});
+var layer = new Kinetic.Layer();
+
+var rect = new Kinetic.Rect({
+    name: 'rect',
+    x: 50,
+    y: 50,
+    width: 25,
+    height: 25,
+    fill: 'black',
+    draggable: true
+});
+var circ = new Kinetic.Circle({
+    id: 'circ',
+    x: 100,
+    y: 100,
+    radius: 5,
+    fill: 'red'
+});
+
+layer.add(rect);
+layer.add(circ);
+stage.add(layer);
