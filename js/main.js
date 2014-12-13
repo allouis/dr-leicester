62615329100c117c465a220be643ea65d6b070d4
benji6 <benhall22@hotmail.co.uk>
2014-12-13 08:38:01 +0000

    tidy main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
-var Kinetic = require('kinetic');
-var Events = require('minivents');
 var backgroundView = require('./lib/backgroundView.js');
 var groundView = require('./lib/groundView.js');
-var playerView = require('./lib/playerView.js');
-var playerModel = require('./lib/playerModel.js');
+var Kinetic = require('kinetic');
 var Layer = require('./lib/layer.js');
-
-var layer = Layer.layer;
+var playerModel = require('./lib/playerModel.js');
+var playerView = require('./lib/playerView.js');
 var stageView = require('./lib/stageView.js');
 
-var pubsub = new Events();
-
+var layer = Layer.layer;
 var stage = stageView.stage;
-
 var background = backgroundView.background;
 var ground = groundView.ground;
 var player = playerView.player;
