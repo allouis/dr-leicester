37a3aa49faf05b3120fa24353ded05bfedd2ebe7
benji6 <benhall22@hotmail.co.uk>
2014-12-13 09:10:33 +0000

    move code out of main.js and into layer.js

--- js/lib/layer.js
+++ js/lib/layer.js
@@ -1,4 +1,15 @@
 var Kinetic = require('kinetic');
+
+var backgroundView = require('./backgroundView.js');
+var groundView = require('./groundView.js');
+
 var layer = new Kinetic.Layer();
+
+var ground = groundView.ground;
+var background = backgroundView.background;
+
+layer.add(background);
+layer.add(ground);
+
 
 exports.layer = layer;
--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
-var backgroundView = require('./lib/backgroundView.js');
-var groundView = require('./lib/groundView.js');
 var Kinetic = require('kinetic');
+
 var Layer = require('./lib/layer.js');
-var playerModel = require('./lib/player/playerModel.js');
-var playerView = require('./lib/player/playerView.js');
 var stageView = require('./lib/stageView.js');
 
 var layer = Layer.layer;
 var stage = stageView.stage;
-var background = backgroundView.background;
-var ground = groundView.ground;
-var player = playerView.player;
 
-layer.add(background);
-layer.add(ground);
+var playerModel = require('./lib/player/playerModel.js');
+var playerView = require('./lib/player/playerView.js');
+var player = playerView.player;
 layer.add(player);
 
 stage.add(layer);
